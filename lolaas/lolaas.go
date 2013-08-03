af983fe214220481103d4ced36f6e8f77f204eb9
Christopher Freeman <cwfreeman@gmail.com>
2013-08-03 02:04:41 -0500

    Added 'ol' handling. Still very very ugly.

--- lolaas/lolaas.go
+++ lolaas/lolaas.go
@@ -8,11 +8,13 @@ import (
 )
 
 var lolRegex *regexp.Regexp
+var loRegex *regexp.Regexp
 var olthRegex *regexp.Regexp
 func init() {
     http.HandleFunc("/lol/", lolHandler)
     http.HandleFunc("/", handler)
-    lolRegex, _ = regexp.Compile("(.*[abcdfghkpst])o(.*)")
+    lolRegex, _ = regexp.Compile("(.*[abcdfghkpst])o([^l].*)")
+    loRegex, _ = regexp.Compile("(.*[abcdfghkpst])(ol.*)")
     olthRegex, _ = regexp.Compile("(.*bo)(th.*)")
 }
 
@@ -20,6 +22,9 @@ func lolify(in string) string {
     out := olthRegex.ReplaceAllString(in, "${1}l${2}")   
     if out == in {
         out = lolRegex.ReplaceAllString(in, "${1}lol${2}")
+        if out == in {
+            out = loRegex.ReplaceAllString(in, "${1}l${2}")
+        }
     }
     return out
 }
